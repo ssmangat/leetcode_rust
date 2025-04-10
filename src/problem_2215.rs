91a121810dc2e938c8a0346967507f0eadf7ed21
ssmangat <sukhmangat7@live.com>
2025-04-10 09:03:37 -0400

    Adding sort for l1 and l2 in problem 2215

--- src/problem_2215.rs
+++ src/problem_2215.rs
@@ -1,12 +1,14 @@
 use std::collections::HashSet;
-pub struct Solution{}
+pub struct Solution {}
 
-impl Solution{
-    pub fn difference_of_two_arrays(nums1:Vec<i32>,nums2:Vec<i32>) -> Vec<Vec<i32>> {
+impl Solution {
+    pub fn difference_of_two_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<Vec<i32>> {
         let s1: HashSet<i32> = nums1.iter().cloned().collect();
         let s2: HashSet<i32> = nums2.iter().cloned().collect();
-        let l1: Vec<i32> = s1.difference(&s2).cloned().collect();
-        let l2: Vec<i32> = s2.difference(&s1).cloned().collect();
-        vec![l1,l2]
+        let mut l1: Vec<i32> = s1.difference(&s2).cloned().collect();
+        let mut l2: Vec<i32> = s2.difference(&s1).cloned().collect();
+        l1.sort();
+        l2.sort();
+        vec![l1, l2]
     }
 }
